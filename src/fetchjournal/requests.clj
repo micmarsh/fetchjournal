90f1ed4ad0aae293701141771f2ef0ebc132e337
Michael Marsh <michael@michael-Aspire-5755.(none)>
2013-01-06 19:57:14 -0500

    siging function complete, probably does not work

--- src/fetchjournal/requests.clj
+++ src/fetchjournal/requests.clj
@@ -3,10 +3,34 @@
             [clojure.string :only [join]]
             [fetchjournal.secrets :as secrets]))
 
+(defn sha1-str [s]
+"blatently copy pasted from https://gist.github.com/1472865"
+  (->> (-> "sha1"
+           java.security.MessageDigest/getInstance
+           (.digest (.getBytes s)))
+       (map #(.substring
+              (Integer/toString
+               (+ (bit-and % 0xff) 0x100) 16) 1))
+       (apply str)))
+
+
+(defn sign [params]
+    (let [  paramstring (join (map #(params %) (sort (keys params))))
+            prepended (str secrets/private_key paramstring)
+            signature (sha1-str prepended)]
+            (merge params {:signature signature})
+    ))
 
 (defn notes-url[username]
     (let [base-url "http://www.fetchnotes.com"]
         (join "/" [base-url "authors" username "notes"])))
 
 (defn upload-note [text]
-   (str (client/get (notes-url secrets/username))))
+   (str (client/post (notes-url secrets/username)
+        {:accept :json,
+        :form-params
+        (sign {
+            :text text
+            :public_key secrets/public_key
+            })
+        })))
